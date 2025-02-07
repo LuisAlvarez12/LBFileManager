8a46fa818a6f1fda828314c1c0c1bcdf87c4aebf
Luis Alvarez <Lalvarez1223@gmail.com>
2025-02-06 20:06:13 -0500

    Initial Commit

--- Package.swift
+++ Package.swift
@@ -5,17 +5,28 @@ import PackageDescription
 
 let package = Package(
     name: "LBFileManager",
+    platforms: [
+        .macOS("14"),
+        .watchOS("6.2"),
+        .iOS("17"),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "LBFileManager",
             targets: ["LBFileManager"]),
     ],
+    dependencies: [
+        .package(name: "LBFoundation", url: "https://github.com/LuisAlvarez12/LBFoundation.git", from: "2.0.2"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "LBFileManager"),
-
+            name: "LBFileManager",
+            dependencies: [
+                .product(name: "LBFoundation", package: "LBFoundation"),
+            ]
+        ),
     ]
 )
